2ef3fdde9369a80a4f36a151c6590a16a30ea3c1
thienlao <72984943+thenlao@users.noreply.github.com>
2020-12-02 18:23:41 +0300

    Исправляю баги залив 1

--- USER.BOT/USER.BOT/FormMassComment.Designer.cs
+++ USER.BOT/USER.BOT/FormMassComment.Designer.cs
@@ -30,9 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMassComment));
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -45,6 +43,8 @@
             this.label10 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +52,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Snow;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(619, 299);
+            this.button1.Location = new System.Drawing.Point(603, 327);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 55);
             this.button1.TabIndex = 0;
@@ -60,37 +60,22 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(36, 334);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(196, 334);
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(15, 347);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(187, 73);
             this.textBox2.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(33, 315);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 16);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "ID получателя";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(193, 315);
+            this.label2.Location = new System.Drawing.Point(18, 327);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(153, 16);
             this.label2.TabIndex = 4;
@@ -131,7 +116,7 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(414, 334);
+            this.numericUpDown1.Location = new System.Drawing.Point(274, 347);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             99,
             0,
@@ -151,7 +136,7 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(411, 315);
+            this.label6.Location = new System.Drawing.Point(271, 328);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(114, 16);
             this.label6.TabIndex = 9;
@@ -178,9 +163,10 @@
             // label9
             // 
             this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label9.Location = new System.Drawing.Point(208, 379);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(116, 13);
+            this.label9.Size = new System.Drawing.Size(134, 13);
             this.label9.TabIndex = 12;
             this.label9.Text = "Откомментрировано:";
             // 
@@ -205,8 +191,32 @@
             this.textBox3.Location = new System.Drawing.Point(603, 52);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox3.Size = new System.Drawing.Size(185, 201);
             this.textBox3.TabIndex = 15;
+            this.textBox3.Text = "https://vk.com/id418227022\r\nhttps://vk.com/id56929156";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(12, 304);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(230, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Вы не заполнили поле для текста!!!!!";
+            this.label11.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(629, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID получателя";
             // 
             // FormMassComment
             // 
@@ -214,6 +224,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label10);
@@ -228,7 +239,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "FormMassComment";
             this.Text = "FormMassComment";
@@ -242,9 +252,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -257,5 +265,7 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label1;
     }
 }
--- USER.BOT/USER.BOT/FormMassComment.cs
+++ USER.BOT/USER.BOT/FormMassComment.cs
@@ -29,59 +29,86 @@ namespace USER.BOT
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            string screen_name = textBox3.Lines[0];
-            
-            for(int i = 0; i< textBox3.Lines.Length; i = i+1 )
+            if (textBox2.Text == "")
             {
-                textBox1.Text = textBox3.Lines[i];
-                Thread.Sleep(1000);
+                label11.Visible = true;
+                label11.BackColor = Color.Red;
+                label2.BackColor = Color.Red;
+                textBox2.BackColor = Color.Red;
                 Application.DoEvents();
+                Thread.Sleep(1000);
+                label11.Visible = false;
+                label11.BackColor = Color.White;
+                label2.BackColor = Color.White;
+                textBox2.BackColor = Color.White;
+                return;
 
             }
-
-
-            string[] ID = screen_name.Split(new[] { "https://vk.com/" }, StringSplitOptions.RemoveEmptyEntries);
-            string request = "https://api.vk.com/method/users.get?user_ids=" + ID[0] + "&" + access_token + "&v=5.124";
-            WebClient clienT = new WebClient();
-            string AnsweR = clienT.DownloadString(request);
-
-
-            UsersGet ug = JsonConvert.DeserializeObject<UsersGet>(AnsweR);
-            string Request = "https://api.vk.com/method/wall.get?" + "owner_id=" + ug.response[0].id.ToString() + "&" +
-                          access_token + "&v=5.124";
-            string Answer = clienT.DownloadString(Request);
-
-
-            WallGet wg = JsonConvert.DeserializeObject<WallGet>(Answer);
-            // label1.Text = AnsweR;
-            int Posts = 0;
-            // labelFam.Text = wg.response.items[0].text;
-            textBox3.Lines[0] = ug.response[0].id.ToString();
-            progressBar1.Value = 0;
-            foreach (WallGet.Item item in wg.response.items)
+            
+            
+            for (int i = 0; i < textBox3.Lines.Length; i = i + 1)
             {
-                Posts = Posts + 1;
-                if (Posts > numericUpDown1.Value)
+                if (textBox3.Lines[i] == "")
                 {
-                    break;
+                    continue;
                 }
-                string RequeST = "https://api.vk.com/method/wall.createComment?" + "owner_id=" + ug.response[0].id.ToString() + "&post_id=" + item.id + "&message=" + textBox2.Text + "&" + access_token + "&v=5.124";
-                WebClient clieNT = new WebClient();
-                string AnswER = clieNT.DownloadString(RequeST);
-                Thread.Sleep(1000);
+                //if(textBox3.Lines[i] = )
+                //{
+                //    
+                //}
+                //"https://vk.com/id435786543"
+                //"https://vk./com/Id435786543"
+                //"https:" "vk" "comid435786543"
 
-                //label1.Text = AnswER;
-                label7.Text = Posts.ToString();
+
+                string screen_name = textBox3.Lines[i];
+                string[] ID = screen_name.Split(new[] { "https://vk.com/" }, StringSplitOptions.RemoveEmptyEntries);
+                int LAST = ID.Length - 1;
+                string request = "https://api.vk.com/method/users.get?user_ids=" + ID[0] + "&" + access_token + "&v=5.124";
+                WebClient clienT = new WebClient();
+                string AnsweR = clienT.DownloadString(request);
+
+
+                UsersGet ug = JsonConvert.DeserializeObject<UsersGet>(AnsweR);
+                string Request = "https://api.vk.com/method/wall.get?" + "owner_id=" + ug.response[0].id.ToString() + "&" +
+                              access_token + "&v=5.124";
+                string Answer = clienT.DownloadString(Request);
+
+
+                WallGet wg = JsonConvert.DeserializeObject<WallGet>(Answer);
+                int Posts = 0;
+                textBox3.Lines[i] = ug.response[0].id.ToString();
+                progressBar1.Value = 0;
+                foreach (WallGet.Item item in wg.response.items)
+                {
+                    Posts = Posts + 1;
+                    if (Posts > numericUpDown1.Value)
+                    {
+                        break;
+                    }
+                    string RequeST = "https://api.vk.com/method/wall.createComment?" + "owner_id=" + ug.response[0].id.ToString() + "&post_id=" + item.id + "&message=" + textBox2.Text + "&" + access_token + "&v=5.124";
+                    WebClient clieNT = new WebClient();
+                    string AnswER = clieNT.DownloadString(RequeST);
+                    Thread.Sleep(1000);
+
+                    //label1.Text = AnswER;
+                    label7.Text = Posts.ToString();
+                    Application.DoEvents();
+                    label8.Text = numericUpDown1.Value.ToString();
+                    progressBar1.Maximum = (int)numericUpDown1.Value;
+                    progressBar1.Value = Posts;
+
+                }
+
+
+
+                Thread.Sleep(1000);
                 Application.DoEvents();
-                label8.Text = numericUpDown1.Value.ToString();
-                progressBar1.Maximum = (int)numericUpDown1.Value;
-                progressBar1.Value = Posts;
-                
+
             }
-           
-            
-            
+
+
+
         }
     }
 }
